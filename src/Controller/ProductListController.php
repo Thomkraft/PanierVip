093476815eeccbadd493f31c4dbf7ab154859e26
Koenig Thomas <thomas.koenig@etu.univ-lyon1.fr>
2025-03-11 16:51:14 +0100

    a

--- src/Controller/ProductListController.php
+++ src/Controller/ProductListController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/product/list')]
+#[Route('/product_list')]
 final class ProductListController extends AbstractController
 {
     #[Route(name: 'app_product_list_index', methods: ['GET'])]
